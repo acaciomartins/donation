9db02b0df966ba24a92de33ded21766b5eac496e
acaciomartins <acacio.am@gmail.com>
2019-10-20 19:11:34 -0300

    implementacao front

---
+++ front/donationApp/src/pages/main.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class Main extends Component {
+    static navigationOptions = {
+        title: 'Doações'
+    };
+    
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>Doações</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        justifyContent: 'center',
+        alignItems: "center"
+    },
+    text: {
+        fontWeight: "bold",
+        color: '#FFF',
+        fontSize: 20
+    }
+
+})
